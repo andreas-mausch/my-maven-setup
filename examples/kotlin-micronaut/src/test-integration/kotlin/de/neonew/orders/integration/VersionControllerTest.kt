39feb05c54dc23095ebf59e0c434658c13b72e0e
neonew <neonew@gmail.com>
2026-09-15 17:00:17 +0200

    test: use default transaction handling for version endpoint

--- examples/kotlin-micronaut/src/test-integration/kotlin/de/neonew/orders/integration/VersionControllerTest.kt
+++ examples/kotlin-micronaut/src/test-integration/kotlin/de/neonew/orders/integration/VersionControllerTest.kt
@@ -9,7 +9,7 @@ import jakarta.inject.Inject
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-@MicronautTest(transactional = false)
+@MicronautTest
 class VersionControllerTest {
   @Inject @field:Client("/") lateinit var client: HttpClient
 
