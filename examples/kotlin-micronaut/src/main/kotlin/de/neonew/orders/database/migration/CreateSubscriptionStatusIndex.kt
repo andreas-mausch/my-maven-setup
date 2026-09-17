939d48725fbe141d32aa91c6fa676aecf9672786
neonew <neonew@gmail.com>
2026-09-17 14:54:32 +0200

    refactor: use index definition for rollback

--- examples/kotlin-micronaut/src/main/kotlin/de/neonew/orders/database/migration/CreateSubscriptionStatusIndex.kt
+++ examples/kotlin-micronaut/src/main/kotlin/de/neonew/orders/database/migration/CreateSubscriptionStatusIndex.kt
@@ -1,14 +1,13 @@
 package de.neonew.orders.database.migration
 
 import com.mongodb.client.MongoDatabase
-import com.mongodb.client.model.IndexOptions
-import com.mongodb.client.model.Indexes
 import de.neonew.orders.OrderSubscription
 import io.micronaut.data.model.PersistentEntity
 import io.micronaut.data.mongodb.operations.MongoCollectionNameProvider
 import io.mongock.api.annotations.ChangeUnit
 import io.mongock.api.annotations.Execution
 import io.mongock.api.annotations.RollbackExecution
+import org.bson.Document
 
 @ChangeUnit(id = "create-subscription-status-index", order = "001", author = "neonew")
 class CreateSubscriptionStatusIndex(collectionNameProvider: MongoCollectionNameProvider) {
@@ -17,17 +16,11 @@ class CreateSubscriptionStatusIndex(collectionNameProvider: MongoCollectionNameP
 
   @Execution
   fun execute(database: MongoDatabase) {
-    database
-        .getCollection(collectionName)
-        .createIndex(Indexes.ascending("status"), IndexOptions().name(INDEX_NAME))
+    database.getCollection(collectionName).createIndex(Document("status", 1))
   }
 
   @RollbackExecution
   fun rollback(database: MongoDatabase) {
-    database.getCollection(collectionName).dropIndex(INDEX_NAME)
-  }
-
-  private companion object {
-    const val INDEX_NAME = "idx_order_subscription_status"
+    database.getCollection(collectionName).dropIndex(Document("status", 1))
   }
 }
