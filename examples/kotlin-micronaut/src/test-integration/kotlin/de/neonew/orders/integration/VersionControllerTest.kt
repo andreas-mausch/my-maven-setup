0c311aff4622dd2ec7b7e845e1e0cafd2cd1b473
neonew <neonew@gmail.com>
2026-09-18 14:54:05 +0200

    feat: manage JSON Unit assertions

--- examples/kotlin-micronaut/src/test-integration/kotlin/de/neonew/orders/integration/VersionControllerTest.kt
+++ examples/kotlin-micronaut/src/test-integration/kotlin/de/neonew/orders/integration/VersionControllerTest.kt
@@ -1,12 +1,11 @@
 package de.neonew.orders.integration
 
-import io.micronaut.core.type.Argument
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
-import org.assertj.core.api.Assertions.assertThat
+import net.javacrumbs.jsonunit.assertj.assertThatJson
 import org.junit.jupiter.api.Test
 
 @MicronautTest
@@ -18,13 +17,10 @@ class VersionControllerTest {
     val response =
         client
             .toBlocking()
-            .retrieve(
-                HttpRequest.GET<Any>("/version"),
-                Argument.mapOf(String::class.java, Any::class.java),
-            )
+            .retrieve(HttpRequest.GET<Any>("/version"), String::class.java)
 
-    assertThat(response).containsOnlyKeys("version")
-    assertThat(response["version"]).isInstanceOf(String::class.java)
-    assertThat(response["version"] as String).matches("[0-9a-f]{7}")
+    assertThatJson(response) {
+      node("version").isString.matches("[0-9a-f]{7}")
+    }
   }
 }
